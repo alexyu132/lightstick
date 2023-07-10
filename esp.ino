be5194f7d0690ee2226ad01688bc82b85e8df771
Anthony Li <antli.inbox@gmail.com>
2023-07-09 19:33:55 -0700

    Add LED strip code

--- esp.ino
+++ esp.ino
@@ -1,18 +1,24 @@
 #include "WiFi.h"
 #include "HTTPClient.h"
+#include "FastLED.h"
 
 #define NUM_LEDS 91
 #define MAX_IMAGE_COLS 100
 #define WIFI_SSID "Wokwi-GUEST"
 #define WIFI_PASSWORD ""
 #define WIFI_CHANNEL 6
+#define LED_PIN 2
 
 uint8_t image[MAX_IMAGE_COLS][NUM_LEDS][3];
 // Actual image width will be known after we finish reading stream
 size_t image_width = 0; 
+uint8_t curr_render_col = 0;
+CRGB leds[NUM_LEDS];
 
 void setup() {
+  
   Serial.begin(9600);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   Serial.print("Connecting to WiFi");
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD, WIFI_CHANNEL);
@@ -55,8 +61,27 @@ void setup() {
   Serial.print("Finished loading image with width: ");
   Serial.print(image_width);
   Serial.println("");
+
+
+
+  leds[0] = CRGB(255, 0, 0);
+  FastLED.show();
 }
 
+void setLedsFromImageCol(uint8_t imageCol) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(image[imageCol][i][0], image[imageCol][i][1], image[imageCol][i][2]);
+  }
+  FastLED.show();
+}
+
+
 void loop() {
+  if (curr_render_col == image_width - 1) {
+    delay(3000);
+    curr_render_col = 0;
+  }
+  setLedsFromImageCol(curr_render_col);
+  curr_render_col++;
   delay(100);
 }

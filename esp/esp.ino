61985a0158ba2249b11a6f66ebe99553e2471397
Alex Yu <alexyu@umd.edu>
2023-07-17 21:02:50 -0700

    Read all bytes in one chunk to fix cutoff issue

--- esp/esp.ino
+++ esp/esp.ino
@@ -3,18 +3,23 @@
 #include "FastLED.h"
 
 #define NUM_LEDS 91
-#define MAX_IMAGE_COLS 100
+#define MAX_IMAGE_COLS 200
 #define WIFI_SSID "ssid"
 #define WIFI_PASSWORD "password"
 #define LED_PIN 16
 
-byte image[MAX_IMAGE_COLS][NUM_LEDS][3];
+#define BYTES_PER_COLUMN NUM_LEDS*3
+
+uint8_t image[MAX_IMAGE_COLS * NUM_LEDS * 3];
+
 // Actual image width will be known after we finish reading stream
-size_t image_width = 0; 
-uint8_t curr_render_col = 0;
+size_t image_width = 0;
+unsigned int curr_render_col = 0;
 CRGB leds[NUM_LEDS];
 
 void setup() {
+  memset(image, 0, sizeof(image));
+
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -28,33 +33,36 @@ void setup() {
   }
 
   delay(3000);
-  
+
   HTTPClient http;
-  http.begin("http://antlimain.s3.us-west-2.amazonaws.com/image_data.bin");
-  
+  http.begin("http://antlimain.s3.us-west-2.amazonaws.com/neon.bin");
+
   int httpCode = http.GET();
 
   WiFiClient *stream = http.getStreamPtr();
-  uint8_t value;
 
-  while (http.connected() && (stream->available()) && image_width < MAX_IMAGE_COLS) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      stream->readBytes(image[image_width][i],3);
-      if (!stream->available()) {
-        break;
-      }
-    }
-    image_width++;
-  }
+  // Dump the raw bytes for the image into the image array
+  // image_width stores num bytes read after this function call
+  image_width = stream->readBytes(image, sizeof(image));
+
+  // Get ceiling of bytes divided by bytes per column to get true image width
+  image_width = (image_width + BYTES_PER_COLUMN - 1) / (BYTES_PER_COLUMN);
 
   leds[0] = CRGB(255, 0, 0);
   FastLED.setBrightness(35);
   FastLED.show();
 }
 
-void setLedsFromImageCol(uint8_t imageCol) {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(image[imageCol][i][0], image[imageCol][i][1], image[imageCol][i][2]);
+void setLedsFromImageCol(unsigned int col) {
+  unsigned int offset = col * BYTES_PER_COLUMN;
+
+  // Store the pixel number separately to avoid a division in loop
+  uint8_t pixel = 0;
+
+  // Each group of 3 bytes represents R,G,B values for a pixel
+  for (int i = 0; i < NUM_LEDS * 3; i+=3) {
+    leds[pixel] = CRGB(image[offset+i],image[offset+i+1],image[offset+i+2]);
+    pixel++;
   }
   FastLED.show();
 }
@@ -67,7 +75,9 @@ void loop() {
     delay(500);
     curr_render_col = 0;
   }
+  
   setLedsFromImageCol(curr_render_col);
+  
   curr_render_col++;
   delay(50);
 }

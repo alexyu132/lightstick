f36da66bfe5f021479f641a650af9b7ad27ff6f6
Anthony Li <antli.inbox@gmail.com>
2023-08-10 22:03:34 -0700

    Handle upload

--- esp/esp.ino
+++ esp/esp.ino
@@ -14,12 +14,15 @@ uint8_t image[MAX_IMAGE_COLS * NUM_LEDS * 3];
 
 // Actual image width will be known after we finish reading stream
 size_t image_width = 0;
+size_t dataArrayIndex = 0;
 unsigned int curr_render_col = 0;
 CRGB leds[NUM_LEDS];
 
 AsyncWebServer server(80);
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting system");
   memset(image, 0, sizeof(image));
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
@@ -31,23 +34,49 @@ void setup() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html");
   });
+  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest * request) {
+    // Set the handler for binary data uploads
+    request->send(200);
+  }, handleBinaryUpload);
 
   server.begin();
 
-  delay(3000);
-
-  WiFiClient *stream = http.getStreamPtr();
-
-  // Dump the raw bytes for the image into the image array
-  // image_width stores num bytes read after this function call
-  image_width = stream->readBytes(image, sizeof(image));
-
-  // Get ceiling of bytes divided by bytes per column to get true image width
-  image_width = (image_width + BYTES_PER_COLUMN - 1) / (BYTES_PER_COLUMN);
 
   leds[0] = CRGB(255, 0, 0);
   FastLED.setBrightness(35);
   FastLED.show();
+  Serial.println("Finished the setup");
+}
+
+void handleBinaryUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+  Serial.println("Handling the binary upload");
+  if (!index) {
+    // Handle the start of the upload
+    // You can create/open a file here to save the uploaded data
+    Serial.println("Upload started");
+    dataArrayIndex = 0;  // Reset the array index
+  }
+
+  if (len > 0) {
+    // Handle incoming binary data
+    // data is the data buffer
+    // len is the size of the current chunk
+    Serial.println("Data incoming");
+    if (dataArrayIndex + len <= sizeof(image)) {
+      // Copy the incoming data into the array
+      memcpy(image + dataArrayIndex, data, len);
+      dataArrayIndex += len;
+    } else {
+      Serial.println("Array is full, data will be truncated");
+    }
+  }
+
+  if (final) {
+    // Handle the end of the upload
+    // Close the file and perform any necessary processing
+    Serial.println("Upload finished");
+    request->send(200, "text/plain", "File upload successful");
+  }
 }
 
 void setLedsFromImageCol(unsigned int col) {
